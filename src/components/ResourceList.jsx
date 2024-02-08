d691d04dc599dbb9cd3db9e33d934ddbe32d9391
Mkhue <mkhue47@gmail.com>
2024-02-08 22:33:12 +0200

    app initialization

---
+++ src/components/ResourceList.jsx
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    id: 1,
+    cardName: "card1",
+    expiryDate: "11/11/23",
+    issuer: "Barclays",
+  },
+  {
+    id: 2,
+    cardName: "card2",
+    expiryDate: "11/11/23",
+    issuer: "Barclays",
+  },
+  {
+    id: 3,
+    cardName: "card3",
+    expiryDate: "11/11/23",
+    issuer: "Barclays",
+  },
+  {
+    id: 4,
+    cardName: "card4",
+    expiryDate: "11/11/23",
+    issuer: "Barclays",
+  },
+  {
+    id: 5,
+    cardName: "card5",
+    expiryDate: "11/11/23",
+    issuer: "Barclays",
+  },
+];
